3e5857ed51f29863f13a535062a9246c6d8dbce4
timmyjinks <tysonjjenkins.github@gmail.com>
2024-12-30 03:49:50 -0700

    Update dal in the backend

--- backend/app.py
+++ backend/app.py
@@ -12,14 +12,33 @@ CORS(app)
 def insert_notes():
     user_name = request.form.get("user_name")
     image = request.files["image"].stream.read()
+
+    if user_name == None:
+        return "Invalid username"
+
     text = pyimagesearch.image_to_string(image).strip()
-    inserted_id = dal.insert_one("user_name", text)
+    inserted_id = dal.insert_one(user_name, text)
     print(inserted_id)
-    return "text"
+    return "200 YG2G"
 
 
 @app.route("/notes/<user_name>", methods=["GET"])
 def get_notes(user_name):
-    print(user_name)
     result = dal.find_all_by_user(user_name)
     return jsonify(result)
+
+
+@app.route("/update_notes", methods=["POST"])
+def edit_notes():
+    id = request.get_json()["_id"]
+    new_note = request.get_json()["new_note"]
+    print(id)
+    result = dal.update_one(id, new_note)
+    return "200 YG2G"
+
+
+@app.route("/delete_notes", methods=["POST"])
+def delete_notes():
+    id = request.get_json()["_id"]
+    result = dal.delete_one(id)
+    return "200 YG2G"
--- backend/dal.py
+++ backend/dal.py
@@ -2,7 +2,6 @@
 import pymongo
 from bson import ObjectId
 
-# CRUD
 client = pymongo.MongoClient("mongodb://root:password@localhost:27017/")
 db = client["user_notes"]
 notes_col = db["notes"]
@@ -14,14 +13,8 @@ def insert_one(user_name: str, notes: str):
     return result.inserted_id
 
 
-# def insert_many(user_name: str, notes: str):
-#     notes_data = {"user_name": user_name, "notes": notes}
-#     result = notes_col.insert_one(notes_data)
-#     result.inserted_id
-
-
 def find_one(id: str):
-    query = {"__id": ObjectId(id)}
+    query = {"_id": ObjectId(id)}
     data = notes_col.find_one(query)
     return data
 
@@ -31,33 +24,26 @@ def find_all():
     return data
 
 
-def find_note_id(user_name: str, note: str):
-    query = {"user_name": user_name, "note": note}
-    data = notes_col.find_one(query, {"__id": 1})
-    return data
-
-
 def find_all_by_user(user_name: str):
     query = {"user_name": user_name}
-    data = notes_col.find(query, {"_id": 0, "user_name": 0})
-    return data.to_list()
+    data = notes_col.find(query)
+    data_new = []
+    for x in data:
+        data_new.append(
+            {"_id": str(x["_id"]), "user_name": x["user_name"], "note": x["note"]}
+        )
+
+    return data_new
 
 
 def update_one(id: str, notes_new: str):
-    query = {"__id": ObjectId(id)}
-    data_new = {"__id": ObjectId(id), "note": notes_new}
-
+    query = {"_id": ObjectId(id)}
+    data_new = {"$set": {"note": notes_new}}
     result = notes_col.update_one(query, data_new)
     return result.modified_count
 
 
 def delete_one(id: str):
-    query = {"__id": ObjectId(id)}
+    query = {"_id": ObjectId(id)}
     result = notes_col.delete_one(query)
     return result.deleted_count
-
-
-# def delete_many(user_name: str, notes: str):
-#     notes_data = {"user_name": user_name, "notes": notes}
-#     result = notes_col.insert_one(notes_data)
-#     result.inserted_id
