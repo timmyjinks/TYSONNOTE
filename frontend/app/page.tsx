ea279bb254352841d840cfaf8a8e2d4bf2375d93
timmyjinks <tysonjjenkins.github@gmail.com>
2024-12-31 22:22:32 -0700

    Add homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,51 @@
-import { LoginForm } from "@/components/Auth";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 export default function Home() {
+  async function handle(formData: FormData) {
+    const response = await fetch("http://localhost:8000/get_note", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.blob();
+    const url = window.URL.createObjectURL(data);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "note.txt");
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode?.removeChild(link);
+  }
+
   return (
     <div>
-      <LoginForm />
+      <header className="flex justify-between border-b ecoration-white">
+        <div className="text-xl m-5">
+          <Link href="/">BITCH</Link>
+        </div>
+        <div>
+          <Link href="/login">
+            <Button className="m-5">Login</Button>
+          </Link>
+          <Link href="/sign_up">
+            <Button className="m-5">Sign Up</Button>
+          </Link>
+        </div>
+      </header>
+      <div className="h-screen flex justify-center items-center">
+        <div className="m-5 w-[80%]">
+          <form className="flex flex-col" action={handle} method="POST">
+            <div className="flex w-1/2 m-auto">
+              <Input type="file" name="image" accept=".jpg, .png, .jpeg" />
+              <Button type="submit">Send</Button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
